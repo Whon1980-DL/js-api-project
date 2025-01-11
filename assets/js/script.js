099bfa4ee110fe29741169e3ce327d3b73e3b4cb
Supitpong Wongsaroj <ewrctysf@students.codeinstitute.net>
2025-01-11 19:16:12 +0000

    Add code to post form and wait for return promise then display error if any

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,50 @@ document.getElementById("submit").addEventListener("click", e => postForm(e)); /
 
 async function postForm(e) {
     const form = new FormData(document.getElementById("checksform"));
-
-    for (let entry of form.entries()) { /* if you want to confirm  that the form has captured correctly. 
+    // This for loop is to test if the form is working
+    //for (let entry of form.entries()) { 
+        /* if you want to confirm  that the form has captured correctly. 
         Then the formData object has several default  methods that allow us to manipulate the data. 
         One of these, is the entries method. Which  we can iterate through to see the form entries.
         */
-            console.log(entry);
+            //console.log(entry);
+        // }
+    const response = await fetch(API_URL, { // As fecth() return a promise we need to use await
+                    method: "POST", // method and header here are the second argument
+                    headers: {
+                            "Authorization": API_KEY,
+            },
+                    body: form, //attach the form as the body of the request
+         })
+
+    const data = await response.json();
+
+    if (response.ok) {
+         displayErrors(data);
+    } else {
+        throw new Error(data.error)
     }
+}
+
+function displayErrors(data) {
+    
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`
+        }
+    }
+    document.getElementById("resultsModalTitle").innerHTML = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+
 }
 
 async function getStatus(e) {
